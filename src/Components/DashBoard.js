f44cc54f997aca5a18cbfc9f98e9d01f61c76006
vijipriya110 <vijipriya110@gmail.com>
2024-03-04 17:28:15 +0530

    done

--- src/Components/DashBoard.js
+++ src/Components/DashBoard.js
@@ -5,7 +5,7 @@ const DashBoard = () => {
   return (
   <Base
   title={"Welocome to Dashboard"}
-  description={"Here CURD Operation of Student Data"}
+  description={"WECOME TO URL-SHORTNER"}
   >
   <h3>This one is Autherized .,First you have to <b>signIn</b></h3>
   </Base>
